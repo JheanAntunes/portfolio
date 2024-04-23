1f0d65c9ce342df5c7c2b4c93374ed85b54d83ba
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-04-23 20:31:02 -0300

    :sparkles:	feat: add Header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,32 @@
+import { GitHubLogoIcon, LinkedInLogoIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
+import Menu from '../menu'
+
+const Header = () => {
+  return (
+    <header className="container grid h-14 grid-cols-[1fr_1px_1fr] items-center border-b border-b-neutral-500">
+      <div>
+        <span>JheanAntunesLopes@gmail.com</span>
+      </div>
+      <div className="h-full bg-neutral-500 dark:bg-neutral-50">
+        {/* decorator */}
+      </div>
+      <nav className="flex justify-between">
+        <div>{/* Button Theme */}</div>
+        <div className="flex">
+          <Link href="#" className="block py-2 pl-2 pr-[10px]">
+            <LinkedInLogoIcon width={20} height={20} />
+          </Link>
+          <Link href="#" className="block py-2 pl-[10px] pr-2">
+            <GitHubLogoIcon width={20} height={20} />
+          </Link>
+        </div>
+        <div>
+          <Menu />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Header

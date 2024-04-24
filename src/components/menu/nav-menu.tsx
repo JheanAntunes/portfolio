478ece11d2b6a39fa59b9a61b67f27c46eb664cf
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-04-24 00:36:45 -0300

    :lipstick:	styles: CSS melhorando o estilo do menu

--- src/components/menu/nav-menu.tsx
+++ src/components/menu/nav-menu.tsx
@@ -15,10 +15,10 @@ const NavMenu = () => {
       variants={menuSlide}
       className="fixed right-0 top-0 z-10 h-svh bg-neutral-900 text-neutral-50"
     >
-      <div className="relative flex h-full flex-col justify-between p-28">
-        <div className="mt-20 flex flex-col gap-3 text-6xl">
-          <div className="mb-10 border-b border-b-neutral-500 text-xs uppercase">
-            <p>Menu</p>
+      <div className="relative flex h-full w-80 flex-col gap-8 md:w-[420px]">
+        <div className="mt-8 flex flex-col items-center gap-5 text-6xl">
+          <div className="border-b border-b-neutral-500 text-xs uppercase">
+            <p className="">Menu</p>
           </div>
 
           {dataLinks.map((data, index) => {
@@ -28,7 +28,7 @@ const NavMenu = () => {
           })}
         </div>
 
-        <div className="flex w-full gap-10 text-xs">
+        <div className="flex w-full items-center justify-center gap-10 text-xs">
           <Link href="#">Github</Link>
           <Link href="#">LinkedIn</Link>
         </div>

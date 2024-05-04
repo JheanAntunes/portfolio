2c12162735a16dc7dc75bb8db60e90607d13bffa
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-05-04 14:15:08 -0300

    :sparkles:	feat: modificando o navbar para ficar fixo na parte inferior e adaptar animação

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -42,17 +42,17 @@ export const FloatingNav = ({ navItems, className }: FloatingNavProps) => {
       <motion.div
         initial={{
           opacity: 1,
-          y: -100
+          y: 100
         }}
         animate={{
-          y: visible ? 0 : -100,
+          y: visible ? 0 : 100,
           opacity: visible ? 1 : 0
         }}
         transition={{
           duration: 0.2
         }}
         className={cn(
-          'fixed inset-x-0  top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white py-2 pl-8 pr-2  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black',
+          'fixed inset-x-0  bottom-5 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-white px-5 py-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:border-white/[0.2] dark:bg-black',
           className
         )}
       >

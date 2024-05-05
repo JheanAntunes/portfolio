3efd002eac09d670a4976e2ff21140277dacb40e
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-05-05 15:25:41 -0300

    :sparkles:	feat: add sections in page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import SectionInitial from '@/components/sections/section-initial'
+import SectionProject from '@/components/sections/section-project'
+
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <SectionInitial />
+      <SectionProject />
+      <div className="h-svh w-full">d</div>
+    </main>
+  )
 }

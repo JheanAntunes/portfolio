736ec0530731c89288ab0365b223d530aab89f9e
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-05-05 15:25:13 -0300

    :sparkles:	feat: add section-projects

---
+++ src/components/constants/data-projects.ts
@@ -0,0 +1,58 @@
+import ImgEcommerce from '@/assets/images/image-ecommerce.png'
+import ImgStorybook from '@/assets/images/image-storybook.png'
+import ImgTanstackQuery from '@/assets/images/image-tanstack-query.png'
+import ImgTipografia from '@/assets/images/image-tipografia.png'
+import ImgZustand from '@/assets/images/image-zustand.png'
+import { StaticImageData } from 'next/image'
+export type DataProject = {
+  image: StaticImageData
+  title: string
+  description: string
+  linkToProject: string
+  linkToGithub: string
+}
+
+export const dataProjects: DataProject[] = [
+  {
+    title: 'E-commerce',
+    description:
+      'O projeto combina funcionalidades essenciais de e-commerce com uma interface amigável, visando aumentar a satisfação do cliente e impulsionar as vendas.',
+    image: ImgEcommerce,
+    linkToProject:
+      'https://ecommerce-7bv7u0yc3-jheanantunes-projects.vercel.app/',
+    linkToGithub: 'https://github.com/JheanAntunes/ecommerce'
+  },
+  {
+    title: 'Storybook Shadcn-ui',
+    description:
+      'Este projeto foi uma oportunidade de mergulhar profundamente na biblioteca Storybook, explorando e aplicando o vasto repositório de componentes do Shadcn-ui.',
+    image: ImgStorybook,
+    linkToProject:
+      'https://65711ecf32bae758b457ae34-uryqbzvojc.chromatic.com/?path=/docs/shadcn-button--docs',
+    linkToGithub: 'https://github.com/JheanAntunes/storybook-shadcn'
+  },
+  {
+    title: 'Zustand: Refatoração e Inovação',
+    description:
+      'Projeto Zustand, enfrentei o desafio de refatorar o componente "mails" do Shadcn-ui, substituindo a biblioteca Jotai pelo Zustand.',
+    image: ImgZustand,
+    linkToProject: 'e-mails-pearl.vercel.app',
+    linkToGithub: 'https://github.com/JheanAntunes/zustand'
+  },
+  {
+    title: 'Projeto Query',
+    description:
+      'Este projeto representa uma jornada de aprofundamento nos fundamentos teóricos e práticos da biblioteca tanstack/query',
+    image: ImgTanstackQuery,
+    linkToProject: 'queryposts.vercel.app',
+    linkToGithub: 'https://github.com/JheanAntunes/react-query'
+  },
+  {
+    title: 'Projeto Tipografia',
+    description:
+      'Inspirado pela arte e ciência da tipografia, este projeto é um reflexo da minha imersão em um blog especializado no assunto. Dediquei-me a explorar e praticar uma variedade de estilos tipográficos...',
+    image: ImgTipografia,
+    linkToProject: 'https://tipografia.vercel.app/',
+    linkToGithub: 'https://github.com/JheanAntunes/tipografia'
+  }
+]
---
+++ src/components/sections/section-project.tsx
@@ -0,0 +1,75 @@
+'use client'
+import { MotionDiv } from '@/lib/framer-motion-components'
+import { useScroll, useTransform } from 'framer-motion'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useRef } from 'react'
+import { dataProjects } from '../constants/data-projects'
+import { buttonVariants } from '../ui/button'
+
+const SectionProject = () => {
+  const targetRef = useRef<HTMLDivElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: targetRef
+  })
+
+  const x = useTransform(scrollYProgress, [0, 1], ['1%', '-95%'])
+  return (
+    <section ref={targetRef} className="container md:relative md:h-[300svh]">
+      <div className="md:sticky md:inset-0 md:flex md:h-svh md:items-center md:overflow-hidden">
+        <MotionDiv
+          style={{ x, y: '-40px' }}
+          className="flex flex-col gap-8 md:flex-row"
+        >
+          {dataProjects.map(
+            ({ image, title, description, linkToGithub, linkToProject }) => (
+              <div
+                key={title}
+                className="flex-shrink-0 space-y-5 rounded-xl border border-solid border-neutral-500 md:h-[460px] md:w-[450px]"
+              >
+                {/* Card */}
+                <div className="relative m-5 h-52 overflow-hidden rounded-xl">
+                  <Image
+                    src={image}
+                    alt={description}
+                    fill
+                    className="object-cover"
+                    sizes="100vh"
+                  />
+                </div>
+                <div className="flex h-52 flex-col justify-between gap-5 overflow-hidden px-5 pb-5">
+                  <div>
+                    <h2 className="text-2xl">{title}</h2>
+                    <p className="text-base">{description}</p>
+                  </div>
+                  <div className="flex gap-5">
+                    <Link
+                      target="_blank"
+                      referrerPolicy="no-referrer"
+                      href={linkToProject}
+                      className={buttonVariants({
+                        className: 'flex-grow uppercase'
+                      })}
+                    >
+                      Projeto
+                    </Link>
+                    <Link
+                      target="_blank"
+                      referrerPolicy="no-referrer"
+                      href={linkToGithub}
+                      className={buttonVariants({ variant: 'secondary' })}
+                    >
+                      Código
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            )
+          )}
+        </MotionDiv>
+      </div>
+    </section>
+  )
+}
+
+export default SectionProject

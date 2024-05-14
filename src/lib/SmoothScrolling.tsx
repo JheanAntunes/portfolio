a4dc4cfa50bbbc7bc74403a2ce9e1f559259639f
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-05-14 14:34:03 -0300

    :broom:	cleanup: removendo prop do provedor ReactLenis

--- src/lib/SmoothScrolling.tsx
+++ src/lib/SmoothScrolling.tsx
@@ -3,7 +3,7 @@ import { ReactLenis } from '@studio-freight/react-lenis'
 
 function SmoothScrolling({ children }: React.PropsWithChildren) {
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5 }}>
       {children}
     </ReactLenis>
   )

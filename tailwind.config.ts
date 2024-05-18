015ff722b9116794a43652a292593019ce1eb9e3
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-05-18 14:33:40 -0300

    :sparkles:	feat: add custom class with bg-image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
 } = require('tailwindcss/lib/util/flattenColorPalette')
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors')
+
 const config = {
   darkMode: ['class'],
   content: [
@@ -92,8 +95,22 @@ const config = {
     function ({ matchUtilities, theme }: any) {
       matchUtilities(
         {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          'bg-dot-thick': (value: any) => ({
+          'bg-grid': (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`
+          }),
+          'bg-grid-small': (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
+            )}")`
+          }),
+          'bg-dot': (value: unknown) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`
+          }),
+          'bg-dot-thick': (value: unknown) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
             )}")`

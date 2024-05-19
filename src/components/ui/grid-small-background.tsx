0d25e0a6bf081c99aa2e3a42a3141d7889c5cd16
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-05-19 14:58:41 -0300

    :lipstick:	styles: eslint-prettier

--- src/components/ui/grid-small-background.tsx
+++ src/components/ui/grid-small-background.tsx
@@ -1,6 +1,6 @@
 export function GridSmallBackground({ children }: React.PropsWithChildren) {
   return (
-    <div className="dark:bg-grid-small-white/[0.2] bg-grid-small-black/[0.2] relative w-full bg-background">
+    <div className="relative w-full bg-background bg-grid-small-black/[0.2] dark:bg-grid-small-white/[0.2]">
       {children}
     </div>
   )
